95eab952d9ac860cb463553878cf19383f7dbeb2
Tad <tad@TAD-PC.localdomain>
2020-05-11 15:06:09 -0500

    finish login component

---
+++ src/pages/sign-in-and-sign-up/signInAndSignUp.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './signInAndSignUp.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignInAndSignUpPage = () => (
+    <div className='signInAndSignUp'>
+        <SignIn />
+    </div>
+);
+
+export default SignInAndSignUpPage;
