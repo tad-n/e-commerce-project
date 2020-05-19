b759d06eb1bdd8a1c469ef09386f15b6b920f362
Tad <tad@TAD-PC.localdomain>
2020-05-19 16:41:09 -0500

    finish signinsignup page

--- src/pages/sign-in-and-sign-up/signInAndSignUp.component.jsx
+++ src/pages/sign-in-and-sign-up/signInAndSignUp.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './signInAndSignUp.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInAndSignUpPage = () => (
     <div className='signInAndSignUp'>
         <SignIn />
+        <SignUp />
     </div>
 );
 
