0cae54d15ad8298c740994a38ad33d939ce179c1
Tad <tad@TAD-PC.localdomain>
2020-05-21 16:14:40 -0500

    add redux

---
+++ src/redux/user/user-reducer.js
@@ -0,0 +1,17 @@
+const INITISL_STATE = {
+    currentUser: null
+}
+
+const userReducer = (state = INITISL_STATE, action) => {
+    switch (action.type) {
+        case 'SET_CURRENT_USER':
+            return {
+                ...state,
+                currentUser: action.payload
+            }
+        default:
+            return state;
+    }
+}
+
+export default userReducer;
